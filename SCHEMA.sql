31298491131825bf8ae471aba5e8144f239ed866
Usuario-PC\Juandi <juadival@espol.edu.ec>
2017-12-19 20:35:02 -0500

    Añadido el Schema. Creada la tabla Ingeniero y parte de la tabla Gasto.

---
+++ SCHEMA.sql
@@ -0,0 +1,11 @@
+create table Ingeniero(
+	RUCIng char(13) NOT NULL,
+    nombreIng varchar (50) NOT NULL,
+    direccionIng varchar (100) NOT NULL,
+    telefono char(10) NOT NULL,
+    primary key (RUCIng));
+    
+create table Gasto(
+	idGasto varchar(30)
+    
+);
