91ce60fdea2a4c9decd61d384d97db45bdbff9b8
Juandi Vallejo <juadival@espol.edu.ec>
2018-01-12 23:32:53 -0500

    Arreglado un problema de archivos, subido el .jar de MySQL Connector y el .sql de inserción de datos.

---
+++ Inserción datos.sql
@@ -0,0 +1,18 @@
+use taller;
+
+#Inserción de Ingeniero
+insert into ingeniero values(0917947053, 'Carlos Vallejo', 'Puerto Azul', 0994755228);
+SELECT 
+    *
+FROM
+    ingeniero;
+    
+#Inserción de Gasto
+insert into gasto values(00, 'Alimentación', 16.50, '2007-01-10', 0917947053);
+select * from gasto;
+
+#Inserción de Cliente
+insert into cliente values(0815946523, 'Xavier Game', 'The Point',  0956111324);
+select * from cliente;
+
+#Inserción de Bote
